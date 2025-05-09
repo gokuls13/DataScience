ec3c6d97273fa716facb04bdc617112720cb2077
gokuls13 <gokuls13102001@gmail.com>
2025-05-09 23:21:15 +0530

    Update streamlit_app.py

--- streamlit_local/streamlit_app.py
+++ streamlit_local/streamlit_app.py
@@ -4,7 +4,7 @@ import joblib
 
 #st.write("Code Change!!")
 #Load the model 
-model = joblib.load("pipe.pkl")
+model = joblib.load("streamlit_local/pipe.pkl")
 
 #Function to make predictions 
 def predict(data):
